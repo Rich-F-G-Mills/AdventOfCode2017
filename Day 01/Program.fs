879ae0c396f290a25e02e71577bc45e6c11c781c
Richard Mills <richardfgmills@gmail.com>
2021-12-20 13:09:39 +0000

    Add project files.

---
+++ Day 01/Program.fs
@@ -0,0 +1,15 @@
+﻿
+open System
+open System.IO
+open FSharpx
+
+
+[<EntryPoint>]
+let main _ =
+
+    let digits =
+        File.ReadAllText "Inputs.txt"
+        |> List.ofSeq
+        |> List.map (Convert.ToString >> int)        
+            
+    0
